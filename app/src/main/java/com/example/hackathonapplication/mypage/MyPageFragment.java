fee04eca936ce262a324bf07f70496cc9a199bb7
YoungBaeKim <canoe918@gmail.com>
2020-12-05 16:08:04 +0900

    mypage import modify

--- app/src/main/java/com/example/hackathonapplication/mypage/MyPageFragment.java
+++ app/src/main/java/com/example/hackathonapplication/mypage/MyPageFragment.java
@@ -1,6 +1,8 @@
 package com.example.hackathonapplication.mypage;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
